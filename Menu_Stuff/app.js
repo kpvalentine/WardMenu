2d173040c47341797ead1467083748f43a821179
Will Pinkston <william@pinkston.com>
2016-10-27 14:56:51 -0600

    added nicknames and quotes

--- Menu_Stuff/app.js
+++ Menu_Stuff/app.js
@@ -28,6 +28,16 @@ function peopleAdd ($http) {
 
 }
 
+function getNickName() {
+    $("#newName").click(function() {
+                var randval = Math.floor(Math.random() * 110);
+                var url = "../getnickname?q=" + randval;
+                $.getJSON(url,function(data) {
+                        console.log(data);
+                });
+        });
+}
+
 function mainCtrl ($scope, $http, peopleAdd) {
 
   $scope.people = []
@@ -43,6 +53,7 @@ function mainCtrl ($scope, $http, peopleAdd) {
     })
 
 $scope.addPeople = function() {
+    $scope.name = $scope.name + getNickName();
   var formData = {name:$scope.Name,avatarUrl:$scope.Url,lastname:$scope.LastName,phonenumber:$scope.PhoneNumber};
   console.log(formData);
   var peopleURL = 'people';
@@ -60,5 +71,25 @@ $scope.addPeople = function() {
     console.log("Post failed");
   });
 }
+//get new quote when the page is reloaded
+$(Document).ready(function() {
+                $("#quoteDiv").html("");
+                var randval = Math.floor(Math.random() * 20) + 1;
+                var url = "../getquote?q=" + randval;
+                //console.log(url);
+                $.getJSON(url,function(data) {
+//                        console.log(data);
+                        $("#quoteSpan").html(data);
+//                    document.getElementById("quoteSpan").innerHTML = data;
+                });
+        });
+    //fetch new nickname when a person's name is submitted
+//        $("#newName").click(function() {
+//                var randval = Math.floor(Math.random() * 110);
+//                var url = "../getnickname?q=" + randval;
+//                $.getJSON(url,function(data) {
+//                        console.log(data);
+//                });
+//        });
 
 }
--- Menu_Stuff/index.js
+++ Menu_Stuff/index.js
@@ -15,11 +15,29 @@ router.get('/people', function(req, res) {
         res.send(people);
 });
 
-var politics = "https://zlzlap7j50.execute-api.us-east-1.amazonaws.com/prod";
-router.get('/politics', function(req,res) {
-  console.log("In politics");
-  request(politics).pipe(res);
+//fetch a random quote from quoteList.txt
+router.get('/getquote', function(req,res,next) {
+        fs.readFile(__dirname + '/quoteList.txt',function(err,data) {
+                if (err) throw err;
+                var citationReturn = data.toString().split("\n");
+                var citeIndex = req.query.q - 1;
+                var jsonresult = [];
+                jsonresult.push({words:citationReturn[citeIndex]});
+                res.status(200).json(jsonresult);
+        });
 });
+//fetch a random nickname from nicknames.txt
+router.get('/getnickname', function(req,res,next) {
+        fs.readFile(__dirname + '/nicknames.txt',function(err,data){
+                if (err) throw err;
+                var nameReturn = data.toString().split("\n");
+                var nameIndex = req.query.q - 1;
+                var jsonName = [];
+                jsonName.push({names:nameReturn[nameIndex]});
+                res.status(200).json(jsonName);
+        });
+});
+
 
 router.post('/people', function(req, res) {
     console.log("In People Post");
